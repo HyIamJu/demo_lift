fd211274422b0936d884fc6f9006d597c039981c
junan <92712852+HyIamJu@users.noreply.github.com>
2025-01-03 11:38:42 +0700

    ehancment fullscreen

--- README.md
+++ README.md
@@ -14,3 +14,19 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+di ras pi nya pake snapp_installer  terus cocokin versi flutter juga sekarang pake : 3.22.3
+
+di laptop yang mau nge run device nya :
+"dart pub global activate snapp_cli"
+"lalu add devices terus tambahkan konfig yang di minta"
+
+cara cek devices yang ada di flutter kita  :
+"flutter devices"
+run nya lgsg aja pake nama device nya
+"flutter run -d pi-4b"
+
+kalau run di minta acces karena minta password ssh nya kita harus konfig run nya :
+ ketik "ssh-copy-id erilsan@192.168.225.114"
+baru tes lagi ke ssh harusnya dia ga perlu ketik password lagi.
+ ketik "ssh erilsan@192.168.225.114"
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
 import 'services/locator.dart';
@@ -18,11 +19,33 @@ void main() async {
   ]);
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // ----------------------------------------------
+  // window manager setup
+  // ----------------------------------------------
+
+  // Inisialisasi window manager
+  await windowManager.ensureInitialized();
+
+  // Set window options
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1920, 1080), // Ukuran layar penuh
+    center: true,
+    backgroundColor: Colors.transparent,
+    title: 'Kiosk Mode',
+    fullScreen: true,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.setFullScreen(true); // Paksa fullscreen
+    await windowManager.setAlwaysOnTop(true); // Pastikan selalu di atas
+    await windowManager.show();
+  });
   runApp(MyApp());
 }
